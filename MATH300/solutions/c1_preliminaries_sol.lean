d1b50339f9b180a08c552d9d9f8026eba17a20c0
CodingComputer2023 <146797173+CodingComputer2023@users.noreply.github.com>
2023-11-13 12:42:27 -0800

    Update c1_preliminaries_sol.lean
    finished 1.1, 1.3, 1.4

--- MATH300/solutions/c1_preliminaries_sol.lean
+++ MATH300/solutions/c1_preliminaries_sol.lean
@@ -4,13 +4,14 @@ section -- 1.1
 -- 1.1 (a): If a and b are integers and a + b = a,
 --          then b = 0
 example (a b : ℤ) (h : a + b = a) : b = 0 := by
-  sorry
+  rw [← add_right_eq_self]
+  exact h
 
 -- 1.1 (b): If a, b, c and d are integers, then
 --          (a + b) + (c + d) = (a + c) + (b + d)
 example (a b c d : ℤ) : (a + b) + (c + d) =
                         (a + c) + (b + d) := by
-  sorry
+  rw [add_assoc, ← add_assoc b, add_comm b c, add_assoc c b, add_assoc a c]
 
 -- 1.1 (c): If a, b, and c are integers such that ac = bc
 --          and c ≠ 0, then a = b.
@@ -25,6 +26,6 @@ example (a b c : ℤ) (h₁ : a * c = b * c) (h₂ : c ≠ 0) :
 -- * You only need to prove (a + b)² = (a² + 2ab) + b²
 --   in this Lean exercise
 example (a b : ℤ) : (a + b)^2 = (a^2 + 2 * a * b) + b^2 := by
-  sorry
+  exact add_sq a b
 
 end -- 1.1
