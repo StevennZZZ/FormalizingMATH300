27da9e099a8a2ef4ad50d040148d91c553e4238a
Steven Zhong <yanzhez@uw.edu>
2023-11-26 22:35:44 -0800

    guide folder created -- 11/26/2023

---
+++ MATH300/guide/c1.lean
@@ -0,0 +1,78 @@
+import Mathlib.Data.Real.Basic
+
+--1.1.01
+
+#eval 1 + 1 = 2
+#eval 1 + 1 = 3
+#eval 2^10 < 2000
+#eval 500 * 20 + 1356^2 - 63794 ≥ 10000
+
+
+--1.1.02
+example {n : ℝ} (h1 : n = 1) : n + 1 = 2 := by
+  rw [h1]
+  norm_num
+
+
+--1.2.01
+example : |-7| = 7 := by
+  norm_num
+
+
+--1.2.02
+example : 3 ∣ 6 := by
+  use 2
+
+
+--1.2.03
+example : Even 20 := by
+  use 10
+
+
+--1.3.01
+example {a b : ℤ} : a + b = b + a := by
+  exact add_comm a b
+
+
+--1.3.02
+example {a b : ℤ} (h : a < b) : a + 5 < b + 5 := by
+  exact add_lt_add_right h 5
+
+
+--1.3.03
+example {a : ℤ} (h : a = 1) : a = 1 := by
+  exact h
+
+
+--1.3.04
+example {a b : ℤ} (h : a + c = b + 0) : a + c = b := by
+  rw [add_zero b] at h
+  exact h
+
+
+--1.3.1.01 (incomplete)
+example {a b c : ℤ} (h : c = a + b) : a = c - b := by
+  sorry
+
+--1.3.1.01 (complete)
+example {a b c : ℤ} (h : c = a + b) : a = c - b := by
+  -- Substitution of Equals:
+  have h1 : c + (-b) = (a + b) + (-b) := by
+    congr
+  -- Associativity of Addition
+  have h2 : c + (-b) = a + (b + (-b)) := by
+    rw [add_assoc a b (-b)] at h1
+    exact h1
+  -- Additive Inverses
+  have h3 : c + (-b) = a + 0 := by
+    rw [add_right_neg b] at h2
+    exact h2
+  -- Additive Identity
+  have h4 : c + (-b) = a := by
+    rw [add_zero a] at h3
+    exact h3
+  -- Symmetry of equality
+  have h5 : a = c + (-b) := by
+    symm at h4
+    exact h4
+  exact h5
---
+++ MATH300/guide/c2.lean
@@ -0,0 +1,106 @@
+import Mathlib.Data.Real.Basic
+
+-- 2.1.01
+#eval 10 > 5
+#eval ¬(10 > 5)
+
+
+-- 2.2.01
+#eval (1 + 1 = 2) ∧ (2 = 0)
+#eval (1 + 1 = 2) ∨ (2 = 0)
+
+
+-- 2.2.02
+example {P Q : Prop} (hp : P) (hq : Q) : P ∧ Q := by
+  constructor
+  · exact hp
+  · exact hq
+
+
+-- 2.2.03
+example {P Q : Prop} (h : P) : P ∨ Q := by
+  left
+  exact h
+
+
+-- 2.2.04
+example {P Q : Prop} (h : Q) : P ∨ Q := by
+  right
+  exact h
+
+
+-- 2.2.05
+example {P Q : Prop} (h : P ∧ Q) : P := by
+  cases' h with hp hq
+  exact hp
+
+
+-- 2.2.06
+example {P Q : Prop} (h : P ∨ Q) : P ∨ Q := by
+  cases' h with hp hq
+  · left
+    exact hp
+  · right
+    exact hq
+
+
+-- 2.3.01
+#eval 10 < 5 → 3 = 4
+
+
+-- 2.3.02 (a)
+example {x : ℝ} (h : x = 2) : x^2 = 4 := by
+  rw [h]
+  norm_num
+
+
+-- 2.3.02 (b)
+example {x : ℝ} : x = 2 → x^2 = 4 := by
+  intro h
+  rw [h]
+  norm_num
+
+
+-- 2.3.03
+example {P Q : Prop} (h1 : P) (h2 : P → Q) : Q := by
+  apply h2
+  exact h1
+
+
+-- 2.3.1.01
+example {P Q : Prop} (h : P → Q) : ¬Q → ¬P := by
+  contrapose!
+  exact h
+
+
+-- 2.3.2.01
+example {P Q : Prop} (h1 :  P → Q) (h2 : Q → P) : P ↔ Q := by
+  constructor
+  · exact h1
+  · exact h2
+
+
+-- 2.4.01 (a)
+example : ∃ n : ℝ, |n| > 0 := by
+  use 1
+  norm_num
+
+
+-- 2.4.01 (b)
+example : ∃ n : ℝ, |n| > 0 := by
+  use -1
+  norm_num
+
+
+-- 2.4.02
+example : ∀ n : ℝ, |n| ≥ 0 := by
+  intro n
+  norm_num
+
+
+-- 2.4.03
+example {n : ℤ} (h : Even n) : Odd (n + 1) := by
+  obtain ⟨k, newh⟩ := h
+  use k
+  rw [newh]
+  ring
