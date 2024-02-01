02bcb3694c86180d42aa95637dc10e771ad8e409
rina-reimer <rinareimer616@gmail.com>
2024-01-31 16:47:25 -0800

    added new file for chapter 7, formalized first excercise for c7

---
+++ MATH300/exercises/c7_relations.lean
@@ -0,0 +1,10 @@
+import Mathlib.Data.Set.Basic
+import Mathlib.Data.Real.Basic
+
+section -- 7.1 For any sets A, B, and C:
+variable {α : Type*}
+variable (A B C : Set α)
+
+-- 7.1 Prove: A × (B ∩ C) = (A × B) ∩ (A × C)
+example A × (B ∩ C) = (A × B) ∩ (A × C) := by
+  sorry
