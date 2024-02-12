6c3fdb265ddc63dafd825c5ad8752130a100fb29
tessgerr <tessgerr@uw.edu>
2024-02-12 12:46:50 -0800

    start problem 6.4

--- MATH300/exercises/c6_induction.lean
+++ MATH300/exercises/c6_induction.lean
@@ -17,7 +17,7 @@ example {a b n : ℕ} (hn : 1 ≤ n) :
 --     Use induction to prove that 9 | 10ⁿ −1.
 example {n : ℕ} (hn : n > 0) : 9 ∣ 10^n - 1 := by
   sorry
-  
+
 example : ∀ (n : ℕ) (hn : 1 ≤ n), 9 ∣ 10^n - 1 := by
   apply Nat.le_induction
   · norm_num
@@ -30,6 +30,17 @@ example : ∀ (n : ℕ) (hn : 1 ≤ n), 9 ∣ 10^n - 1 := by
     have h3 : 10 ^ (k + 1) = 10 * 10 ^ k := by
       exact pow_succ 10 k
     rw[h3]
+    rw [Nat.mul_sub_right_distrib, Nat.sub_eq_of_eq_add]
+    rw [Nat.add_assoc]
+    norm_num
+    rw [Nat.sub_add_cancel]
+    rw [Nat.mul_comm 10 (10 ^ k)]
+    norm_num
+    have h: 1 = 10^0 := by norm_num
+    rw [h]
+    apply Nat.pow_le_pow_of_le_right
+    linarith
+    linarith
 
 
 
@@ -40,6 +51,15 @@ example : ∀ (n : ℕ) (hn : 1 ≤ n), 9 ∣ 10^n - 1 := by
 --     Use induction to prove that 8 |3²ⁿ −1.
 example {n : ℕ} (hn : n > 0) : 8 ∣ 3^(2 * n) - 1 := by
   sorry
+
+example : ∀ (n : ℕ) (hn : 1 ≤ n), 8 ∣ 3^(2 * n) - 1 := by
+  apply Nat.le_induction
+  · norm_num
+  · intro k hn h1
+    rcases h1 with ⟨m, h1⟩
+    use 9 * m + 1
+    have 
+
 
 -- 6.5 Let n be a positive integer.
 --     Use induction to prove that 6 |n³ −n.
